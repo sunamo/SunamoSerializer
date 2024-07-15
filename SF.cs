f0001adfe0074356ec4fe3ac6b56d7605855a8cb
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-15 21:38:29 +0200

    Before renaming all *SH and *SE

--- SF.cs
+++ SF.cs
@@ -515,7 +515,7 @@ public static class SF
             oddelovaciZnak = AllStrings.verbar;
         }
         // Musí tu být none, protože pak když někde nic nebylo, tak mi to je nevrátilo a progran vyhodil IndexOutOfRangeException
-        return SHSplit.Split(var, oddelovaciZnak);
+        return SHSplit.SplitMore(var, oddelovaciZnak);
     }
 
 
--- _sunamo/SunamoStringSplit/SHSplit.cs
+++ _sunamo/SunamoStringSplit/SHSplit.cs
@@ -1,7 +1,7 @@
 namespace SunamoSerializer._sunamo.SunamoStringSplit;
 internal class SHSplit
 {
-    internal static List<string> Split(string p, params string[] newLine)
+    internal static List<string> SplitMore(string p, params string[] newLine)
     {
         return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
